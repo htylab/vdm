7a060f8325f826a2511fb3e3690477795fc7b8b1
kuogeo123 <kuogeo123@gmail.com>
2022-12-08 13:38:23 +0800

    0.0.1
    update

--- vdm/vdm.py
+++ vdm/vdm.py
@@ -20,7 +20,7 @@ def main():
     parser.add_argument('input',  type=str, nargs='+', help='Path to the input image, can be a folder for the specific format(nii.gz)')
     parser.add_argument('-o', '--output', default=None, help='File path for output image, default: the directory of input files')
     parser.add_argument('-b0', '--b0_index', default=None, type=str, help='The index of b0 slice or the .bval file, default: 0 (the first slice)')
-    parser.add_argument('-r', '--resample', default=True, help='Resample to 1.7, default: True')
+    parser.add_argument('-r', '--resample', default=True, type=bool, help='Resample to 1.7, default: True')
     parser.add_argument('-m', '--dmap', action='store_true', help='Producing the virtual displacement map')
     parser.add_argument('-g', '--gpu', action='store_true', help='Using GPU')
 
